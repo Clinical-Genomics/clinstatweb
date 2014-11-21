c358c7ffe787b048426452d084d3026b281a237e
Kenny Billiau <kenny.billiau@scilifelab.se>
2014-11-21 15:46:46 +0100

    Finished a rudimentary runs page

--- clinstat/core/views.py
+++ clinstat/core/views.py
@@ -1,10 +1,13 @@
 # encoding: utf-8
 
+from __future__ import absolute_import, unicode_literals
+
 from flask import Blueprint, url_for
 from flask.ext.sqlalchemy import get_debug_queries
 
 from ..extensions import db
 from ..models import Project, Datasource, Flowcell, Unaligned
+from ..helpers import templated
 
 core = Blueprint('core', __name__, template_folder='templates')
 
@@ -13,6 +16,7 @@ def index():
     return 'Projects #{}'.format(Project.query.all())
 
 @core.route('/runs')
+@templated('runs.html')
 def runs():
     """SELECT YEAR(rundate) AS year, MONTH(rundate) AS month, COUNT(DISTINCT datasource.datasource_id) AS runs, ROUND(SUM(readcounts/2000000),2) AS 'Mil reads'
     FROM datasource
@@ -20,14 +24,16 @@ def runs():
     LEFT JOIN unaligned ON unaligned.flowcell_id = flowcell.flowcell_id
     GROUP BY YEAR(rundate), MONTH(rundate)
     ORDER BY YEAR(rundate), MONTH(rundate);"""
-    rs = ''
-    for row in Datasource.query.\
+    rs = db.session.query(
+                db.func.year(Datasource.rundate),\
+                db.func.month(Datasource.rundate),\
+                db.func.count(Datasource.datasource_id.distinct()),\
+                db.func.round(db.func.sum(Unaligned.readcounts / 20000000), 2)
+            ).\
             join(Flowcell).\
             join(Unaligned).\
             group_by(db.func.year(Datasource.rundate), db.func.month(Datasource.rundate)).\
             order_by(db.func.year(Datasource.rundate), db.func.month(Datasource.rundate)).\
-            all():
-        rs += format(row)
-    print(get_debug_queries())
+            all()
 
-    return rs
+    return dict(out=rs)
