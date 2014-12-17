45d55faf06cb469f46f0ec73cd1feff50c820da4
Kenny Billiau <kenny.billiau@scilifelab.se>
2014-12-17 09:42:26 +0100

    Added a q30 page

--- clinstat/core/views.py
+++ clinstat/core/views.py
@@ -33,10 +33,58 @@ def runs():
                 db.func.round(db.func.sum(Unaligned.readcounts / 2000000), 2).label('mil reads'),\
                 db.func.round(db.func.sum(Unaligned.readcounts) / (db.func.count(Datasource.datasource_id.distinct())*2000000), 1).label('mil reads fc lane')
             ).\
-            join(Flowcell).\
-            join(Unaligned).\
+            outerjoin(Flowcell).\
+            outerjoin(Unaligned).\
             group_by(db.func.year(Datasource.rundate), db.func.month(Datasource.rundate)).\
             order_by(db.func.year(Datasource.rundate).desc(), db.func.month(Datasource.rundate).desc(), db.func.day(Datasource.rundate).desc()).\
             all()
 
     return dict(out=rs)
+
+@core.route('/q30')
+@templated('q30.html')
+def q30():
+    """ SELECT runname, COUNT(DISTINCT datasource.datasource_id) AS runs,
+    flowcellname, lane, SUM(readcounts),
+    ROUND(SUM(readcounts)/(2000000),1) AS "mil reads/fc lane",
+    GROUP_CONCAT(q30_bases_pct*readcounts), datasource.datasource_id, rundate
+    FROM datasource
+    LEFT JOIN flowcell ON datasource.datasource_id = flowcell.datasource_id
+    LEFT JOIN unaligned ON unaligned.flowcell_id = flowcell.flowcell_id
+    GROUP BY unaligned.flowcell_id, lane
+    ORDER BY rundate, flowcellname, lane """
+    rs = db.session.query(
+            Datasource.runname,\
+            db.func.count(Datasource.datasource_id.distinct()).label('runs'),\
+            Flowcell.flowcellname,\
+            Unaligned.lane,\
+            db.func.sum(Unaligned.readcounts),\
+            db.func.round(db.func.sum(Unaligned.readcounts)/(2000000), 1).label('mil reads fc lane'),\
+            db.func.group_concat(Unaligned.q30_bases_pct*Unaligned.readcounts),\
+            Datasource.datasource_id,\
+            Datasource.rundate\
+        ).\
+        outerjoin(Flowcell).\
+        outerjoin(Unaligned).\
+        group_by(Unaligned.flowcell_id, Unaligned.lane).\
+        order_by(Datasource.rundate, Flowcell.flowcellname, Unaligned.lane).\
+        all()
+
+    rows = []
+    for row in rs:
+        q30joined = row[6].split(',')
+        q30sum = 0
+        for q30s in q30joined:
+            q30sum += float(q30s)
+        if int(row[4]) > 0:
+            fcq30 = q30sum/int(row[4])
+        else:
+            fcq30 = 0
+        rows.append( 
+            (row[8], row[0], row[1], row[2], row[3], row[4], row[5], "{0:.2f}".format(fcq30), row[7], "{0:.2f}".format(float(row[5])*float(fcq30)/100) )
+        )
+
+    return dict(out=rows)
+
+
+
